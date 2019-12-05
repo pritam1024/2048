eb455d34eee84971214395e6aadd5da20e177105
Pritam Das <dpritam1024@gmail.com>
2019-12-05 07:29:48 +0530

    Changed title and removed unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import { Row } from "./Components/Row/Row";
 
 const App = () => {
